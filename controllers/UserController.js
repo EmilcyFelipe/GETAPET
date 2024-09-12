4e810852a3f3e466bb867062543d0de878982345
Felipe de Paula <emilcyfelipe@gmail.com>
2024-09-12 05:12:37 -0300

    edit

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,9 +5,10 @@ const bcrypt = require('bcrypt')
 
 const jwt = require('jsonwebtoken')
 
+//helpers
 const createUserToken = require('../helpers/create-user-token')
-
 const getToken = require('../helpers/get-token')
+const getUserByToken = require('../helpers/get-user-by-token')
 
 module.exports = class UserController{
     static async register(req,res){
@@ -139,6 +140,56 @@ module.exports = class UserController{
             return
         }
         res.status(200).json(user)
+        return
+    }
+
+    static async editUser(req,res){
+        const id = req.params.id;
+
+        const token = await getToken(req)
+        const user = getUserByToken(token);
+
+        const {name,email,phone,password, confirmpassword} = req.body;
+        
+        let image = '';
+
+        //validations
+        if(!name){
+            res.status(422).json({message:'O nome é obrigatório'})
+            return
+        }
+        if(!email){
+            res.status(422).json({message:'O e-mail é obrigatório'})
+            return
+        }
+
+        //check if email has already taken
+        const userExists = await User.findById(id);
+
+        if(user.email !== email && userExists){
+            res.status(422).json({message:"Por favor, utilize outro e-mail!"})
+        }
+
+        user.email = email
+
+        
+        if(!phone){
+            res.status(422).json({message:'O telefone é obrigatório'})
+            return
+        }
+        if(!password){
+            res.status(422).json({message:'A senha é obrigatória'})
+            return
+        }
+        if(!confirmpassword){
+            res.status(422).json({message:'A confirmação de senha é obrigatória'})
+            return
+        }
+
+        if(!user){
+            res.status(422).json({message:"Usuário não encontrado!"})
+        }
+
         return
     }
 
